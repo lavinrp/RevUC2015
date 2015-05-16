40b00b9a7bdd653c1646a55d2d4535492d486bfd
lavinrp <lavinrp@gmail.com>
2015-05-16 03:06:29 -0400

    basic functionality for taging variables on the heap

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 #include <string>
-
+#include <vector>
+#include "tag.h"
 
 using namespace std;
+
+template <class C>
+C* IdHeap(C heapVariable, string variableName,vector<Tag> tags, long long lineNumber) {
+
+    /// create the variable on the heap with a copy constructor
+    C* heapReturn = new C(heapVariable);
+
+    ///create a tag for the variable
+    //Tag heapTag(heapReturn, lineNumber, variableName);
+
+    ///return newly created pointer
+    return heapReturn;
+}
 
 int main() {
 
