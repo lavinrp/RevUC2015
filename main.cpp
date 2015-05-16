c360876439e0d0591db1494d6d05e4f2050299d4
lavinrp <lavinrp@gmail.com>
2015-05-16 04:57:00 -0400

    added IdStack function to add tags to variables on the stack. Bug fixees and comments for IdHeap

--- main.cpp
+++ main.cpp
@@ -7,18 +7,31 @@
 using namespace std;
 
 template <class C>
-C* IdHeap(C heapVariable, string variableName,vector<Tag> tags, long long lineNumber) {
+C* IdHeap(C heap_variable, string variable_name, vector<Tag> &tags, long long line_number) {
 
     /// create the variable on the heap with a copy constructor
-    C* heapReturn = new C(heapVariable);
+    C* heap_return = new C(heap_variable);
 
-    auto variableLocation = &(*heapReturn);
+    /// find memory address of variable
+    auto variable_location = &(*heap_return);
 
-    ///create a tag for the variable
-    Tag heapTag(variableLocation, variableName, lineNumber);
+    ///create a tag for the variable and add it to vector of tags
+    Tag heap_tag(variable_location, variable_name, line_number);
+    tags.push_back(heap_tag);
 
     ///return newly created pointer
-    return heapReturn;
+    return heap_return;
+}
+
+template <class C>
+void IdStack(C stack_variable, string variable_name, vector<Tag> &tags, long long line_number) {
+
+    ///find memory location of stack variable
+    auto variable_location = &stack_variable;
+
+    ///create tag for stack variable and add it to vector of tags
+    Tag stack_tag(variable_location, variable_name, line_number);
+    tags.push_back(stack_tag);
 }
 
 int main() {
